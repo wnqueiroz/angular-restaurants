05912e47043e323844d165036fa768c9ec93ff68
William Queiroz <williamdenovaisqueiroz@outlook.com>
2017-12-22 10:12:35 -0200

    Deploy Test

--- server.js
+++ server.js
@@ -6,7 +6,9 @@ const app = express();
 app.use(express.static(__dirname + '/dist'));
 
 app.get('*', function (req, res) {
-	res.sendFile(path.join(__dirname,'/src/index.html'));
+	console.log('Loading request...');
+	console.log('File: ' + __dirname +'/dist/index.html');
+	res.sendFile(__dirname +'/dist/index.html');
 });
 
 // Start the app by listening on the default Heroku port
